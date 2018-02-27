fafb888c5cdfa45eca0586f660361dec8a924d09
Maksym <developerint97@gmail.com>
2018-02-27 15:07:32 +0200

    Update SearchActivity.java

--- app/src/main/java/com/maks/flickrapplication/SearchActivity.java
+++ app/src/main/java/com/maks/flickrapplication/SearchActivity.java
@@ -2,6 +2,11 @@ package com.maks.flickrapplication;
 
 import android.os.Bundle;
 
+/**
+ * Created by GuessWh0o on 06.05.2017.
+ * Email: developerint97@gmail.com
+ */
+
 public class SearchActivity extends BaseActivity {
 
     @Override
